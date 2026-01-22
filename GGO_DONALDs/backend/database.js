e11e080dfb375abb82412908864a6209db4f5910
gabrielgomesortiz <gabrielgomesortiz@gmail.com>
2026-01-21 22:52:47 -0300

    ajuste

--- GGO_DONALDs/backend/database.js
+++ GGO_DONALDs/backend/database.js
@@ -5,7 +5,7 @@ const dbConfig = {
   host: 'localhost',
   port: 5432, // Porta padrão do PostgreSQL
    user: 'postgres', // Usuário padrão do PostgreSQL (ajuste conforme necessário)
-   password: '#',
+   password: 'sem senha',
   
   //user: 'postgres', // Usuário padrão do PostgreSQL (ajuste conforme necessário)
   //password: 'postgres',
